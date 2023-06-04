b274e69d9a1d2c5140edb79d13ccb83a1156a50b
thelordgod <george88512@gmail.com>
2023-06-04 03:27:05 +0300

    finally fizzbuzz

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,15 +1,22 @@
 (setq *print-case* :capitalize)
 
 (setq fizzDict
-	  '((3 . "Fizz")
-		(5 . "Buzz")))
+	  '((3 3. "Fizz")
+		(5 5. "Buzz")
+		(7 7. "Bazz")
+		(11 11. "Bezz")
+		(19 19. "Bizz")
+   ))
 
-;;;(loop for div in fizzDict do
-;;;	  (format t "~a ~%" (car div)))
-
-(defun getFizzBuzzString(num)
-  (loop for div in fizzDict do
-		(when (= 0 (mod num (car div)))
-		  (format t "ass ~a~%" num))))
+ (defun getFizzBuzzString(num)
+  (setq result "")
+  (loop for x in fizzDict do
+    (when (= num (cadr x))
+      (setq result (concatenate 'string result (caddr x)))
+        (setf (cadr x) (+ (cadr x) (car x)))))
+  (when (string-equal result "")
+    (print num))
+  (unless (string-equal result "")
+    (print result)))
 
 (loop for x from 1 to 100 do (getFizzBuzzString x))
